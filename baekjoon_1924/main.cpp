5635485993b206a5bccdd950fa13f6621909538b
WaniFi <skelpd24@gmail.com>
2018-08-17 17:15:01 +0900

    2948 문제 풀이 완료

--- baekjoon_1924/main.cpp
+++ baekjoon_1924/main.cpp
@@ -2,13 +2,14 @@
 #include <string>
 using namespace std;
 
+// 2009³â ¹öÀü
 int m, d, pre_m = 1, pre_d = 1;
 int month[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30 ,31 };
-string days[7] = { "MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN" };
+string days[7] = { "Thursday", "Friday", "Saturday", "Sunday", "Monday", "Tuesday", "Wednesday" };
 
 int main()
 {
-	cin >> m >> d;
+	cin >> d >> m;
 	while (m > pre_m || d >= pre_d + 7) {
 		pre_d += 7;
 		if (pre_d > month[pre_m-1]) {
